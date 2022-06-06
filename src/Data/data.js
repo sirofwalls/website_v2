1dc66036431304d92737eeae1d853921e85e6ea2
Peter Walser <mail@ptrwlsr.com>
2022-06-06 17:40:31 -0500

    Added work section

---
+++ src/Data/data.js
@@ -0,0 +1,61 @@
+import RealEstate from '../assets/realestate.jpg'
+import WorkImg from '../assets/workImg.jpeg'
+
+import Linux from '../assets/linux.png'
+import Docker from '../assets/docker.png'
+import MySQL from '../assets/mysql.png'
+import ReactJS from '../assets/react.png'
+import Node from '../assets/node.png'
+import Cloudflare from '../assets/cloudflare.png'
+import GitHub from '../assets/github.png'
+import MongoDB from '../assets/mongo.png'
+
+export const cardData = [
+    {
+        title: 'First',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: WorkImg,
+    },
+    {
+        title: 'Second',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: RealEstate,
+    },
+    {
+        title: 'Third',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: WorkImg,
+    },
+    {
+        title: 'Fourth',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: RealEstate,
+    },
+    {
+        title: 'Fifth',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: WorkImg,
+    },
+    {
+        title: 'Sixth',
+        demoLink: '/',
+        codeLink: 'https://github.com',
+        image: RealEstate,
+    },
+]
+
+export const techData = [
+    {name: 'Linux', icon: Linux},
+    {name: 'Docker', icon: Docker},
+    {name: 'MySQL', icon: MySQL},
+    {name: 'NodeJS', icon: Node},
+    {name: 'React', icon: ReactJS},
+    {name: 'Cloudflare', icon: Cloudflare},
+    {name: 'GitHub', icon: GitHub},
+    {name: 'Mongo', icon: MongoDB},
+]
